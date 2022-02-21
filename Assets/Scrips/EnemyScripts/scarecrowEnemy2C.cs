eb2f5e314ec75d64d2d2419f99818a31770c55a9
nkc20206055 <ym2022@mailg.denpa.ac.jp>
2022-02-21 09:20:43 +0900

    敵のアニメーション追加

---
+++ Assets/Scrips/EnemyScripts/scarecrowEnemy2C.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scarecrowEnemy2C : MonoBehaviour
+{
+    public GameObject bullet;//発射する弾を入れる
+    public int MaxHP;//最大HP
+    public float Maxattacktime;//
+    Vector3 bulletPos;
+    private SpriteRenderer sr;//自身のspriteを保存
+    private Animator anim = null;
+    private int HP;//この敵のHP
+    private float time;//溜まったら動きための数値
+    void Damage()
+    {
+        anim.Play("damage");
+        HP--;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = MaxHP;
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sr.isVisible)//sr(SpriteRenderer)が画面に入っているとき動く
+        {
+            time += 1 * Time.deltaTime;
+            Debug.Log(time);
+            if (time >= Maxattacktime)
+            {
+                Debug.Log("射撃");
+                var t=Instantiate(bullet);
+                Vector3 ss = transform.position;
+                bulletPos = new Vector3((float)(ss.x+ -0.79),(float)(ss.y+0.64),ss.z);
+                t.transform.position = bulletPos;
+                time = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scrips/bulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+    public float Speed;//弾のスピード
+    public float objectPeel;
+    Vector3 nn;
+    private SpriteRenderer SR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SR.isVisible) {
+            nn.x = objectPeel * Speed * Time.deltaTime;
+            transform.position += nn;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
