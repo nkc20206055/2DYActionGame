026045207d3a799636df1dddd57bc49d0729425b
nkc20206055 <ym2022@mailg.denpa.ac.jp>
2022-02-25 12:16:01 +0900

    プレイヤーのHP機能追加

--- Assets/Scrips/guardController.cs
+++ Assets/Scrips/guardController.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class guardController : MonoBehaviour
 {
     [SerializeField] GameObject CounterObject;
+    public GameObject hpui;//HPのUIを入れる変数
     public float counterTime;//カウンターできる時間
     public float damageTime;//無敵時間
 
     EcColliderController ECC;
+    SpriteRenderer SR;//自分のSpriteRendererを入れる変数
+    int hp,deletehp;//HPの変数
 
     private Animator anim;
     private Slider slider;//ガードゲージのUI
@@ -18,7 +21,8 @@ public class guardController : MonoBehaviour
     private bool MouseSwicth;//マウスが動かせるかどうか
     private bool gadeSwicth;//ガードが起動しているかどうか
     private bool counterSwicth;//
-    private bool damageSwicth;//
+    private bool damageSwicth;//ダメージを食らっているかどうか
+    private bool damageHetSwcith;//ダメージを食らったときにうごく
     void animationcancel()//アニメーションを途中で終わらせるのに使う
     {
         anim.SetBool("counterattack", false);
@@ -31,14 +35,32 @@ public class guardController : MonoBehaviour
     {
         if (damageSwicth == true)
         {
+            //HPを減らす
+            if (damageHetSwcith==true)
+            {
+                for(int i=0;i< deletehp; i++)
+                {
+                    GameObject s=hpui.transform.GetChild(hp-1).gameObject;
+                    s.SetActive(false);
+                    hp--;
+                    Debug.Log(hp);
+                    if (hp<=0)
+                    {
+                        damageSwicth = false;
+                        i = deletehp;
+                    }
+                }
+                deletehp = 0;
+                damageHetSwcith = false;
+            }
             cTime += 1 * Time.deltaTime;
             if (cTime<=0.01f) {
                 gameObject.layer = LayerMask.NameToLayer("PlayerDamge");//レイヤーマスクを変更する
+            }else if (cTime > 0.01f&&cTime<damageTime)
+            {
+
             }
-
-
-
-            if (cTime>= damageTime)
+            else if (cTime>= damageTime)
             {
                 gameObject.layer = LayerMask.NameToLayer("Default");//レイヤーマスクを戻す
                 damageSwicth = false;
@@ -49,6 +71,7 @@ public class guardController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        hp = 10;//最大hpの設定
         anim = GetComponent<Animator>();
         slider = GameObject.Find("guardgage").GetComponent<Slider>();
         MouseSwicth = true;
@@ -58,6 +81,7 @@ public class guardController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //ガードブレイク
         if (sliderS <= 0)
         {
             MouseSwicth = false;
@@ -69,7 +93,8 @@ public class guardController : MonoBehaviour
             slider.value = slider.maxValue;
         }
 
-        if (counterSwicth==true)//カウンターが成功した時
+        //カウンターが成功した時
+        if (counterSwicth==true)
         {
             if (ECC.counterHetSwicth == true)
             {
@@ -82,19 +107,22 @@ public class guardController : MonoBehaviour
             counterSwicth = false;
         }
 
+        //カウンターとガードの操作
         if (MouseSwicth==true) {
-            if (Input.GetMouseButton(1))//右クリックで起動
+            if (Input.GetMouseButton(1))//右クリック押しっぱなし
             {
                 CGtime += 1 * Time.deltaTime;
                 if (CGtime < counterTime) //カウンター確認
                 {
                     //Debug.Log("カウンター中"+CGtime);
+                    gameObject.layer = LayerMask.NameToLayer("PlayerDamge");
                     anim.SetBool("counter", true);
                     CounterObject.SetActive(true);
 
                 } else if (CGtime >= counterTime) //ガード中
                 {
                     //Debug.Log("ガード中");
+                    gameObject.layer = LayerMask.NameToLayer("Default");//レイヤーマスクを戻す
                     anim.SetBool("guard", true);
                     anim.SetBool("counter", false);
                     CounterObject.SetActive(false);
@@ -105,12 +133,14 @@ public class guardController : MonoBehaviour
                 if (CGtime <= 0.05f)//すぐにできないよう猶予を作っている
                 {
                     Debug.Log("カウンターキャンセル");
+                    gameObject.layer = LayerMask.NameToLayer("Default");//レイヤーマスクを戻す
                     anim.SetBool("counter", false);
                     CounterObject.SetActive(false);
                 }
                 else if (CGtime > 0.05f && CGtime < counterTime)//カウンター
                 {
                     Debug.Log("カウンター");
+                    gameObject.layer = LayerMask.NameToLayer("Default");//レイヤーマスクを戻す
                     //anim.SetBool("counterattack", true);
                     anim.SetBool("counter", false);
                     CounterObject.SetActive(false);
@@ -119,6 +149,7 @@ public class guardController : MonoBehaviour
                 else if (CGtime >= counterTime)//ガードを終わらせる
                 {
                     Debug.Log("ガード終了");
+                    gameObject.layer = LayerMask.NameToLayer("Default");//レイヤーマスクを戻す
                     anim.SetBool("guard", false);
                     gadeSwicth = false;
                 }
@@ -127,12 +158,17 @@ public class guardController : MonoBehaviour
         }else
         {
             Debug.Log("ガード中止");
+            gameObject.layer = LayerMask.NameToLayer("Default");//レイヤーマスクを戻す
             CGtime = 0;
         }
 
         
 
         Damage();
+        if (hp<=0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -147,7 +183,8 @@ public class guardController : MonoBehaviour
 
         if (collision.gameObject.tag=="enemyrightattack")
         {
-            damageSwicth = true;
+            damageHetSwcith = true;
+            damageSwicth = true;     
             if (gadeSwicth==true)
             {
                 Debug.Log("敵の弱攻撃を軽減");
@@ -158,25 +195,27 @@ public class guardController : MonoBehaviour
             {
                 Debug.Log("敵の弱攻撃がヒット");
                 //減るのはハート1つ
-
+                deletehp = 2;
             }
         }
         else if (collision.gameObject.tag == "enemyheavyattack")
         {
+            damageHetSwcith = true;
             damageSwicth = true;
             if (gadeSwicth == true)
             {
                 Debug.Log("敵の強攻撃を軽減");
                 //減るのはハート半分でガードゲージが減る
+                deletehp = 1;
                 slider.value -= 20;
                 sliderS = slider.value;
-                Debug.Log(sliderS);
+                //Debug.Log(sliderS);
             }
             else
             {
                 Debug.Log("敵の強攻撃がヒット");
-                //減るのはハート2つ
-
+                //減るのはハート1.5つ
+                deletehp = 3;
             }
         }
     }
